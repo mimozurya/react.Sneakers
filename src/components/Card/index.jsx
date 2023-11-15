e74e7cff80dca688f6c1c6ad5b3978288d4b64de
mimozurya <egor_sidorenko007@mail.ru>
2023-11-16 00:00:04 +0500

    3/7

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,15 +1,22 @@
-function Card() {
+import styles from "./Card.module.scss";
+console.log(styles);
+
+function Card(props) {
+    const onClickButton = () => {
+        alert(123);
+    };
+
     return (
-        <div className="card">
-            <div className="favorite">
+        <div className={styles.card}>
+            <div className={styles.favorite}>
                 <img src="/img/heart-unliked.svg" alt="Unliked" width={32} height={32} />
             </div>
-            <img src="/img/sneakers/1.jpg" alt="Sneakers" width={133} height={112} />
-            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <img src={props.imageUrl} alt="Sneakers" width={133} height={112} />
+            <h5>{props.title}</h5>
             <div className="d-flex justify-between align-center">
                 <div className="d-flex flex-column">
                     <span>Цена:</span>
-                    <b>12 999 руб.</b>
+                    <b>{props.price} руб.</b>
                 </div>
                 <button className="button">
                     <img src="/img/plus.svg" alt="Plus" width={11} height={11} />
